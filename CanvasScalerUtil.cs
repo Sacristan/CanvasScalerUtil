5e60119b07723f4b99dd6fe83b6f9876dab871e0
Girts Kesteris <girts.kesteris@ui.com>
2020-10-12 10:38:37 +0300

    initial commit

---
+++ CanvasScalerUtil.cs
@@ -0,0 +1,34 @@
+﻿/// <summary>
+/// Author: https://github.com/Sacristan
+/// Script to hastily create correct canvas scales. Logic handled at editor's side
+/// </summary>
+
+using UnityEngine;
+
+namespace Sacristan.CanvasScaler
+{
+    [RequireComponent(typeof(Canvas))]
+    public class CanvasScalerUtil : MonoBehaviour
+    {
+        [SerializeField]
+        private uint canvasWidth = 800;
+
+        [SerializeField]
+        private uint canvasHeight = 600;
+
+        [SerializeField]
+        private float canvasWorldSizeInMeters = 1;
+
+        public uint CanvasWidth { get { return canvasWidth; } }
+        public uint CanvasHeight { get { return canvasHeight; } }
+        public float CanvasWorldSizeInMeters { get { return canvasWorldSizeInMeters; } }
+
+        #region MonoBehaviour
+        private void Awake()
+        {
+            Debug.Log("CanvasScalerUtil has nothing to do with realtime logic. Please remove me");
+            Destroy(this);
+        }
+        #endregion
+    }
+}
